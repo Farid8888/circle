13e42aa2e220b92b2497c603dd88880b9461c170
Farid <faridqasimov8@gmail.com>
2022-08-11 15:26:51 +0400

    circle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="aside_footer-info">
+    <div className="footer_btn_one p1"></div>
     </div>
   );
 }
